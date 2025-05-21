381d524451caa1bc553b4ad81cbeb7ba2c5d2c11
Berkan Karayel <berkankarayel.147@gmail.com>
2025-05-21 11:40:19 +0300

    Gerçek tasarım güncellemesi yapıldı

--- proje/api.js
+++ proje/api.js
@@ -3,7 +3,7 @@ class WeatherAPI {
         this.baseUrl = "https://api.openweathermap.org/data/2.5/weather";
         this.apiKey = "40780f584ae83749b62a5335ccf1e583"
     }
-
+    // güncelleme yapıldı.
     async getWeatherInfo(cityName) {
         const response = await fetch(`${this.baseUrl}?q=${cityName}&units=metric&lang=tr&appid=${this.apiKey}`)
         const data = await response.json();
